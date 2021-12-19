c739ad5568e76e9b93784963901145df93d96a80
heweilun <91226024@qq.com>
2021-12-19 21:12:47 +0800

    app.js commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require("http")
+const querystring = require("querystring")
+
+const server = http.createServer((req, res)=>{
+    if(req.method === "GET") {
+        let params = querystring.parse(req.url.split("?")[1]);
+        res.end(JSON.stringify(params))
+    }else if(req.method === "POST") {
+        res.end("post")
+    }
+})
+
+server.listen(3000, ()=>{
+    console.log('success')
+})
+© 2021 GitHub, Inc.
