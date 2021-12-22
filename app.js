e8529b7516d5892e5e4699d6429d83a48966a6a1
heweilun <91226024@qq.com>
2021-12-22 23:43:56 +0800

    开发环境初步搭建

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-const http = require("http")
+
 const querystring = require("querystring")
 
-const server = http.createServer((req, res)=>{
-    if(req.method === "GET") {
-        let params = querystring.parse(req.url.split("?")[1]);
-        res.end(JSON.stringify(params))
-    }else if(req.method === "POST") {
-        console.log("这是dev的修改第三次")
-        res.end("post end")
+const serverHandle = (req, res)=>{
+    //设置返回格式
+    res.setHeader('Content-type', 'application/json')
+    const method = req.method//请求方式
+    const url = req.url//请求路径
+    const path = url.split('?')[0]
+    const query = querystring.parse(url.split('?')[1])//请求参数
+    // process.env.NODE_ENV cross-env安装后可获取
+    const resData = {
+        method,
+        url,
+        path,
+        query
     }
-})
+    if(method === "GET") {
+        res.end(JSON.stringify(resData))
+    }else if(method === "POST") {
+        let postData = ""
+        req.on('data', chunk => {//发送数据
+            postData += chunk.toString()
+        })
+        req.on('end', () => {//数据发送完成
+            resData.postData = postData
+            res.end(JSON.stringify(resData))
+        })
+    }
+}
 
-server.listen(8000, ()=>{
-    console.log('success')
-})
+module.exports = serverHandle
+
